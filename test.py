6f0a61b5f498cf96724e8650e0871f0371462add
Abhay Sharma <abhaysharmafalakata@gmail.com>
2024-06-10 23:52:06 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def sum_array_np(array):
+    return np.sum(array)
+
+# Example usage
+array1 = np.array([1, 2, 3, 4, 5])
+print("Sum of array elements:", sum_array_np(array1))
