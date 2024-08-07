10eebc9a8915731cd81f055b9aa21708b2058b33
Abhay Sharma <abhaysharmafalakata@gmail.com>
2024-08-07 23:39:52 +0530

    Create isHappy.java

---
+++ isHappy.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        
+        return n == 1;
+    }
+}
